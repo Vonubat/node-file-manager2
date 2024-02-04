1daba0129ce05aef0a831235cc74fd7325f395fa
Tabunov Egor <Vonubat@gmail.com>
2024-02-04 16:35:43 +0300

    Add constants

---
+++ src/constants/index.js
@@ -0,0 +1,11 @@
+export const MSGS = {
+    INVALID_INPUT: '\n\x1b[33m **Invalid input** \x1b[0m\n',
+    OPEARTION_FAILED: '\n\x1b[31m **Operation failed** \x1b[0m\n',
+    OPERATION_SUCCESSFUL: '\n\x1b[32m **Opeartion successful!** \x1b[0m\n',
+};
+
+export const ERROR_MSGS = {
+    FILE_DOESNT_EXIST: `File doesn't exist`,
+    FILE_EXISTS: `File exists`,
+    IS_FILE: `Is File`,
+};
